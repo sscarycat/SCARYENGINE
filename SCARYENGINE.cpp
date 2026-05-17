059cd19d129bf55dda065069c4d99a2254dbfab7
thescarycat <sscarycat@proton.me>
2026-05-17 12:20:33 +1000

    Убран namespacestd также добавлена новая функция DrawMenu. Также пофикшен баг с Звонком. теперь он мигает.

--- SCARYENGINE.cpp
+++ SCARYENGINE.cpp
@@ -14,13 +14,14 @@ void SCARYENGINE::clear() {
 }
 
 void SCARYENGINE::sleep_for(int ms) {
-    this_thread::sleep_for(chrono::milliseconds(ms));
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-void SCARYENGINE::make_call(int count, int ms, string text) {
+void SCARYENGINE::make_call(int count, int ms, std::string text) {
     for (int call = 0; call < count; call++) {
+        sleep_for(ms);
         clear();
-        cout << text;
+        std::cout << text;
         sleep_for(ms);
     }
 }
@@ -35,24 +36,35 @@ void SCARYENGINE::print_ascii(const std::string& splash) {
     std::cout << splash;
 }
 
-void SCARYENGINE::play_dialog(const vector<Line>& dg) {
+void SCARYENGINE::play_dialog(const std::vector<Line>& dg) {
     for (size_t x = 0; x < dg.size(); x++) {
         clear();
-        cout << dg[x].text;
+        std::cout << dg[x].text;
         sleep_for(dg[x].ms);
     }
 }
 
 int SCARYENGINE::Random(int min, int max) {
-    static random_device rd;
-    static mt19937 mt(rd());
-    uniform_int_distribution<int> dist(min, max);
+    static std::random_device rd;
+    static std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(min, max);
     return dist(mt);
 }
 
-void SCARYENGINE::type_text(string_view text, int delay) {
+void SCARYENGINE::type_text(std::string_view text, int delay) {
     for (char c : text) {
-        cout << c << flush;
+        std::cout << c << fflush;
         sleep_for(delay);
+    }
+}
+
+void SCARYENGINE::ScaryRENDERER::DrawMenu(const char *splash, const std::vector<std::string>& elements) {
+    while (*splash) {
+        putchar(*splash++);
+    }
+
+
+    for (size_t x = 0; x < elements.size(); x++) {
+        std::cout << elements[x] << std::endl;
     }
 }
